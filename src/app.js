1a004b91c49203f510f0844916bbcc85e5f0412a
joannaarroyo <joliuac@gmail.com>
2019-09-18 10:07:31 -0700

    initial commit - react project scaffolding complete

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//Import components here
+//Import stylesheets here
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Template works!</h1>
+      {/* Add components here*/}
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
